1b25f798bbc1d0ca085dc5afb93c797d0403b165
Dan Croak <dan@statusok.com>
2018-10-29 13:46:42 -0700

    Group client/server code, improve comments

--- examples/react-with-rails-api/heroku.sh
+++ examples/react-with-rails-api/heroku.sh
@@ -2,9 +2,8 @@
 
 set -e
 
+# Prepare dependencies
 cache=${CACHE_DIR:-$HOME}
-
-# ASDF
 export ASDF_DATA_DIR="$cache/.asdf"
 export PATH="$ASDF_DATA_DIR/bin:$ASDF_DATA_DIR/shims:$PATH"
 
@@ -26,12 +25,11 @@ asdf_install() {
 }
 
 # Build Node client
-export NODEJS_CHECK_SIGNATURES=no
-asdf_plugin_update node https://github.com/asdf-vm/asdf-nodejs.git
-asdf_install node 8.9.0
-
 (
   cd client
+  export NODEJS_CHECK_SIGNATURES=no
+  asdf_plugin_update node https://github.com/asdf-vm/asdf-nodejs.git
+  asdf_install node 8.9.0
 
   if [ -d "$cache/node_modules" ]; then
     mv "$cache/node_modules" .
@@ -40,26 +38,24 @@ asdf_install node 8.9.0
   npm install --scripts-prepend-node-path
   npm run build --scripts-prepend-node-path
   cp -R public/ ../server/public/
-
   mv node_modules "$cache"
 )
 
-# Build  Ruby server
-asdf_plugin_update ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf_install ruby 2.5.1
-gem install bundler --no-document
-asdf reshim ruby
-
+# Build Ruby server
 (
   cd server
+  asdf_plugin_update ruby https://github.com/asdf-vm/asdf-ruby.git
+  asdf_install ruby 2.5.1
+  gem install bundler --no-document
+  asdf reshim ruby
   bundle install --without development:test --path="$(gem env gemdir)"
 )
 
-# Copy dependencies
+# Copy dependencies to the build
 mkdir -p "$BUILD_DIR/tmp/cache"
 cp -R "$ASDF_DATA_DIR" "$BUILD_DIR/tmp/cache/.asdf"
 
-# Create startup script to load dependencies
+# Create script to load dependencies at runtime
 mkdir -p "$BUILD_DIR/.profile.d"
 touch "$BUILD_DIR/.profile.d/startup.sh"
 
